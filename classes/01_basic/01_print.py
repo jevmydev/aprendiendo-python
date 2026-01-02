5965db492df5ffc70110cfff0d321e059343bf14
Jeremy <jeremy664k@gmail.com>
2026-01-01 22:57:17 -0300

    Add first class complete

---
+++ classes/01_basic/01_print.py
@@ -0,0 +1,23 @@
+### 
+
+# 01 - print()
+
+# Es un módulo para imprimir mensajes en la consola.
+
+# Recordar:
+# Los archivos serán escritos en snake case (usar guiones bajos para separar palabras).
+# La extensión del archivo será .py
+# Los strings o cadenas de texto se definen usando comillas dobles ("") o simples ('').
+
+###
+
+print("Hello World!")
+print('También puedo usar comillas simples.')
+
+print("Python", "es", "genial") # Por defecto separa los elementos con un espacio y salto de línea al final (\n) 
+print("Python", "es", "brutal", sep="-") # Separamos los elementos con un guion
+
+print("en una línea")
+print ("Esto se imprime", end=" ") # Cambiamos el final del salto de línea (\n) a un espacio
+
+print(42) # Podemos imprimir números
