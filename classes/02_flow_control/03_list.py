3b6ef986591d72caa43f752ed2bf3d7e3f577cea
Jeremy <jeremy664k@gmail.com>
2026-01-06 22:30:54 -0300

    Add list completed (day 6)

--- classes/02_flow_control/03_list.py
+++ classes/02_flow_control/03_list.py
@@ -10,7 +10,7 @@ os.system("clear")
 # Crear listas
 print("Crear listas")
 list_1 = [1, 2, 3, 4, 5] # Lista de enteros
-list_2 = ["a", "b", "c"] # Lista de cadenas
+list_2 = ["manzanas", "pera", "plátano"] # Lista de cadenas
 list_3: list[int | str | float | bool] = [1, "b", 3.5, True] # Lista de tipos mixtos. Opcional el "tipeado"
 
 list_4 = [] # Lista vacía
@@ -20,7 +20,7 @@ matrix = [[1, 2], [2, 3], [4, 5]] # Matriz (lista de listas)
 print(list_1)
 print(list_2)
 print(list_3)
-print(list_4)
+# print(list_4)
 print(list_of_list)
 print(matrix)
 
@@ -32,3 +32,38 @@ print(list_2[1]) # "b" = list_[-2]
 print(list_2[-1]) # "c" (último elemento, cuenta del final hacia el inicio)
 
 print(list_of_list[1][0]) # 3 (primer elemento de la segunda lista)
+
+# Slicing (rebanado) de listas
+
+print("\nSlicing de listas")
+
+print(list_1[1:4]) # [2, 3, 4]
+print(list_1[:3]) # [1, 2, 3]
+print(list_1[3:]) # [4, 5]
+
+print(list_1[:]) # Copia de la misma lista
+
+list_1 = [1, 2, 3, 4, 5, 6, 7, 8]
+print(list_1[::2]) # list_1[desde:hasta:paso (cada cuanto cuenta)]
+print(list_1[::-1]) # Invierte la lista (pasos al revés)
+
+# Modificar una lista
+print("\nModificando lista")
+
+list_1[0] = 9
+print(list_1)
+
+# Añadir elementos
+print("\nAñadir elementos a la lista")
+
+# Forma larga y menos eficiente
+list_1 = [1, 2, 3]
+list_1 = list_1 + [4, 5, 6]
+print(list_1)
+
+# Forma más corta y más eficiente
+list_1 += [7, 8, 9]
+print(list_1)
+
+# Recuperar longitud de una lista
+print("\nLongitud de la lista:", len(list_1))
