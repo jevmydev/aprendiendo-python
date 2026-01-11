881b952f2fdbf41a61144735ed5afcb44d9596ca
Jeremy <jeremy664k@gmail.com>
2026-01-10 23:00:28 -0300

    Add exercises for (day 10)

---
+++ classes/03_loops/02_exercises_loop_for.py
@@ -0,0 +1,85 @@
+###
+# EJERCICIOS (for)
+###
+import os
+os.system("clear")
+
+# Ejercicio 1: Imprimir números pares
+# Imprime todos los números pares del 2 al 20 (inclusive) usando un bucle for.
+print("\nEjercicio 1:")
+numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+for num in numbers:
+    if num % 2 == 0:
+        print(num, end=" ")
+
+# Ejercicio 2: Calcular la media de una lista
+# Dada la siguiente lista de números:
+# numeros = [10, 20, 30, 40, 50]
+# Calcula la media de los números usando un bucle for.
+print("\n\nEjercicio 2:")
+
+numbers = [10, 20, 30, 40, 50]
+sum = 0
+
+for num in numbers:
+    sum += num
+
+average = sum / len(numbers)
+
+print(average)
+
+# Ejercicio 3: Buscar el máximo de una lista
+# Dada la siguiente lista de números:
+# numeros = [15, 5, 25, 10, 20]
+# Encuentra el número máximo en la lista usando un bucle for.
+print("\nEjercicio 3:")
+
+numbers = [15, 5, 25, 10, 20]
+
+for num in numbers:
+    is_max_number = True
+
+    for compare_num in numbers:
+        if num < compare_num:
+            is_max_number = False
+            break
+
+    if is_max_number:
+        print(f"El número máximo es {num}")
+        break
+
+# Ejercicio 4: Filtrar cadenas por longitud
+# Dada la siguiente lista de palabras:
+# palabras = ["casa", "arbol", "sol", "elefante", "luna"]
+# Crea una nueva lista que contenga solo las palabras con más de 5 letras
+# usando un bucle for y list comprehension.
+print("\nEjercicio 4:")
+
+words = ["casa", "arbol", "sol", "elefante", "luna"]
+filter_words = [word for word in words if len(word) > 5]
+
+print(filter_words)
+
+# Ejercicio 5: Contar palabras que empiezan con una letra
+# Dada la siguiente lista de palabras:
+# palabras = ["casa", "arbol", "sol", "elefante", "luna", "coche"]
+# Pide al usuario que introduzca una letra.
+# Cuenta cuántas palabras en la lista empiezan con esa letra (sin diferenciar mayúsculas/minúsculas).
+print("\nEjercicio 5:")
+
+words = ["casa", "arbol", "sol", "elefante", "luna", "coche"]
+user_letter = input("Introduce una letra: ").lower()
+
+count_coincidence = 0
+
+for word in words:
+    first_letter_word = word[0].lower()
+    
+    if user_letter == first_letter_word:
+        count_coincidence += 1
+
+if count_coincidence == 1:
+    print(f"Existe {count_coincidence} palabra que empieza por la letra {user_letter}")
+else:
+    print(f"Existen {count_coincidence} palabras que empiezan por la letra {user_letter}")
