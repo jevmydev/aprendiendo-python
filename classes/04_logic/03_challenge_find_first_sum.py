a9f0f3770297e5c4efc92c7bdbd2bef8a6b9fe7d
Jeremy <jeremy664k@gmail.com>
2026-01-14 22:41:51 -0300

    Refactor 03_challenge with dictionaries (day 14)

--- classes/04_logic/03_challenge_find_first_sum.py
+++ classes/04_logic/03_challenge_find_first_sum.py
@@ -9,19 +9,32 @@ find_first_sum(nums, goal)  # [2, 3]
 import os
 os.system("clear")
 
-def find_first_sum(nums, goal):
-    range_nums = range(len(nums) - 1)
+# def find_first_sum(nums, goal):
+#     range_nums = range(len(nums) - 1)
 
-    for index in range_nums:
-        num = nums[index]
-        next_index = index + 1
+#     for index in range_nums:
+#         num = nums[index]
+#         next_index = index + 1
 
-        for compare_num in nums[next_index:]:
-            total = num + compare_num
+#         for compare_num in nums[next_index:]:
+#             total = num + compare_num
 
-            if total == goal:
-                return [index, nums.index(compare_num, next_index)]
+#             if total == goal:
+#                 return [index, nums.index(compare_num, next_index)]
     
+#     return None
+
+def find_first_sum(nums, goal):
+    seen = {} # diccionario para guardar número e índice
+
+    for index, value in enumerate(nums):
+        missing = goal - value
+
+        if missing in seen:
+            return [seen[missing], index]
+        
+        seen[value] = index
+
     return None
 
 nums = [4, 5, 6, 2]
