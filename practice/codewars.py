dc71288342ddc533accd2a10c7b3e1fe609ab135
Jeremy <jeremy664k@gmail.com>
2026-01-16 23:03:05 -0300

    Add exercises codewars (day 16)

---
+++ practice/codewars.py
@@ -0,0 +1,36 @@
+import re
+
+def fibonacci_reverse(first, second):
+    while first <= second:
+        diff = second - first
+        print(f"first: {first} second: {second} diff: {diff}")
+
+        if diff > first:
+            return first, second
+        
+        second = first
+        first = diff
+
+print(fibonacci_reverse(2440, 3948))
+
+# -----------------------------
+
+def reversed_str(string):
+    return string[::-1]
+
+print(reversed_str("worlds"))
+
+# -----------------------------
+
+def string_to_number(s):
+    return int(s)    
+
+# -----------------------------
+
+def dna_to_rna(dna):
+    pattern = "T"
+    rna = re.sub(pattern, "U", dna)
+
+    return rna    
+
+print(dna_to_rna("TTTT"))
