536d492531ac9911bce10dbe37678fc8dd3a597f
Jeremy <jeremy664k@gmail.com>
2026-01-21 22:26:00 -0300

    Add new challenges of programming (day 21)

--- practice/challenges/programming_challenges.py
+++ practice/challenges/programming_challenges.py
@@ -1,4 +1,6 @@
+import re
 import os
+
 os.system("clear")
 
 print("---------------------------------------")
@@ -72,4 +74,78 @@ print(is_anagram("Roma", "amor"))           # True
 print(is_anagram("roma", "amor "))    # True 
 
 print("---------------------------------------")
+print("Tercer ejercicio - Fibonacci")
 print("---------------------------------------")
+
+###
+# * Escribe un programa que imprima los 50 primeros números de la sucesión
+# * de Fibonacci empezando en 0.
+# * - La serie Fibonacci se compone por una sucesión de números en
+# *   la que el siguiente siempre es la suma de los dos anteriores.
+# *   0, 1, 1, 2, 3, 5, 8, 13...
+###
+
+fibonacci = []
+
+for i in range(51):
+    if len(fibonacci) < 2:
+        fibonacci.append(i)
+        continue
+
+    last_num = fibonacci[i - 1]
+    second_to_last_num = fibonacci[i - 2]
+
+    fibonacci_num = last_num + second_to_last_num
+    fibonacci.append(fibonacci_num)
+
+for num in fibonacci:
+    print(f"{num}", end=", " if num != fibonacci[-1] else "\n")
+
+print("---------------------------------------")
+print("Cuarto ejercicio - Área de un polígono")
+print("---------------------------------------")
+
+###
+# * Crea una única función (importante que sólo sea una) que sea capaz
+# * de calcular y retornar el área de un polígono.
+# * - La función recibirá por parámetro sólo UN polígono a la vez.
+# * - Los polígonos soportados serán Triángulo, Cuadrado y Rectángulo.
+# * - Imprime el cálculo del área de un polígono de cada tipo.
+###
+
+def area_of_polygon(polygon, base, height):
+    polygon = polygon.lower()
+    
+    pattern_square = "cuadrado"
+    is_square = re.search(pattern_square, polygon)
+
+    pattern_rectangle = "rect.ngulo"
+    is_rectangle = re.search(pattern_rectangle, polygon)
+
+    pattern_triangle = "tri.ngulo"
+    is_triangle = re.search(pattern_triangle, polygon)
+
+    area = base * height 
+
+    if is_triangle:
+        area /= 2
+    
+    if is_square or is_rectangle or is_triangle:
+        return f"El área del {polygon} es: {area} u^2"
+
+print(area_of_polygon("Triangulo", base=3, height=4))
+
+print("---------------------------------------")
+print("Quinto ejercicio - Invertir cadenas")
+print("---------------------------------------")
+
+###
+# * Crea un programa que invierta el orden de una cadena de texto
+# * sin usar funciones propias del lenguaje que lo hagan de forma automática.
+# * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+###
+
+def reverse_str(str):
+    return str[::-1]
+
+print(reverse_str("Hola mundo"))
